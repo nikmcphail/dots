63bd29eeabc088609701d29c8fb17e737fb6c516
Nikolas McPhail <nikolasstevenmcphail@gmail.com>
2026-02-14 16:40:18 -0500

    Update Oil.nvim to always show hidden files.

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,12 @@
 return {
-    'stevearc/oil.nvim',
-    --@module 'oil'
-    --@type oil.SetupOpts
-    opts = {},
-    dependencies = { { "nvim-mini/mini.icons", opts={} } },
-    lazy = false,
+	"stevearc/oil.nvim",
+	--@module 'oil'
+	--@type oil.SetupOpts
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	lazy = false,
 }
