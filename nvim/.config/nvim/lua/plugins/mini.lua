fee81a7c3ac5650baf327724e1859c61ebe6fd2d
Nikolas McPhail <nikolasstevenmcphail@gmail.com>
2026-02-14 17:45:18 -0500

    More mini.nvim plugins and add lazy-lock

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,12 +1,22 @@
 return {
-    {
-        'nvim-mini/mini.statusline',
-        version = false,
-        opts = {},
-    },
-    {
-        'nvim-mini/mini.pairs',
-        version = false,
-        opts = {},
-    }
+	{
+		"nvim-mini/mini.statusline",
+		version = false,
+		opts = {},
+	},
+	{
+		"nvim-mini/mini.pairs",
+		version = false,
+		opts = {},
+	},
+	{
+		"nvim-mini/mini.tabline",
+		version = false,
+		opts = {},
+	},
+	{
+		"nvim-mini/mini.starter",
+		version = false,
+		opts = {},
+	},
 }
