825a324bff3e73d469c85e3fc9be5da01a81ebe1
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-19 03:18:47 -0500

    Update blink.lua and treesitter.lua

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -26,29 +26,22 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "default" },
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
+			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
-			documentation = { auto_show = true, auto_show_delay_ms = 250 },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 250,
+				treesitter_highlighting = true,
+				window = { border = "rounded" },
+			},
 			menu = {
 				border = "rounded",
 				winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -30,6 +30,7 @@ return {
 				"markdown_inline",
 				"fish",
 				"zig",
+				"rust",
 			},
 		})
 
