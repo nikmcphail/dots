edaa55a856be3f51a5da85048521d9859033a81c
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-19 14:20:54 -0500

    Move keymaps for oil to Oil.lua

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,23 +2,28 @@ return {
 	"stevearc/oil.nvim",
 	--@module 'oil'
 	--@type oil.SetupOpts
-	opts = {
-		default_file_explorer = true,
-		delete_to_trash = true,
-		view_options = {
-			show_hidden = true,
-		},
-		keymaps = {
-			["<C-c>"] = false,
-			["q"] = "actions.close",
-		},
-		float = {
-			border = "rounded",
-		},
-		confirmation = {
-			border = "rounded",
-		},
-	},
 	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 	lazy = false,
+	config = function()
+		require("oil").setup({
+			default_file_exporer = true,
+			delete_to_trash = true,
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["<C-c>"] = false,
+				["q"] = "actions.close",
+			},
+			float = {
+				border = "rounded",
+			},
+			confirmation = {
+				border = "rounded",
+			},
+		})
+
+		vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>-", "<cmd>Oil --float<CR>", { desc = "Open parent directory (floating)" })
+	end,
 }
