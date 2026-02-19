de0e7583d239ccf931eae9dc73ec33848a570f06
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-19 18:20:05 -0500

    Working on LSP

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -48,17 +48,10 @@ return {
 			},
 		},
 
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			-- add lazydev to your completion providers
-			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
-					score_offset = 100,
-				},
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
 		snippets = { preset = "luasnip" },
