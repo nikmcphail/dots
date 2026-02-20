eee589e981212594b2e575a2f2d901c5b2194fb4
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-20 02:13:33 -0500

    Add flash.nvim to folke.lua

--- nvim/.config/nvim/lua/plugins/folke.lua
+++ nvim/.config/nvim/lua/plugins/folke.lua
@@ -8,4 +8,36 @@ return {
 		"folke/neodev.nvim",
 		opts = {},
 	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
 }
