a2859e74b85bc2028df93be88ad815f78f128655
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-19 20:08:42 -0500

    LSP seems to work now?:w

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,28 +1,9 @@
 return {
 	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
+	dependencies = { "rafamadriz/friendly-snippets" },
 	build = "cargo build --release",
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			version = "2.*",
-			build = (function()
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
-				},
-			},
-			opts = {},
-		},
-	},
-	version = "1.9.1",
+
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
@@ -30,38 +11,22 @@ return {
 		keymap = { preset = "default" },
 
 		appearance = {
-			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
 			documentation = {
-				auto_show = true,
+				auto_show = false,
 				auto_show_delay_ms = 250,
 				treesitter_highlighting = true,
 				window = { border = "rounded" },
 			},
 			menu = {
 				border = "rounded",
-				winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+				winhighlight = "Normal:BlinkCmpDoc,FloatBoardr:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
 			},
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-
-		snippets = { preset = "luasnip" },
-
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	opts_extend = { "sources.default" },
 }
