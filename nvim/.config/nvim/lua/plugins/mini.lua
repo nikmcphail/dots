aa9d6259ac306c719be204d0cc48461c28a472c6
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-21 18:15:39 -0500

    Add mini.ai to mini.lua

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -14,10 +14,25 @@ return {
 	{
 		"nvim-mini/mini.indentscope",
 		version = false,
-		opts = {},
+		opts = {
+			mappings = {
+				goto_top = "[i",
+				goto_bottom = "]i",
+			},
+			options = {
+				border = "both",
+				indent_at_cursor = true,
+			},
+			symbol = "╎",
+		},
 	},
 	{
 		"nvim-mini/mini.tabline",
+		version = false,
+		opts = {},
+	},
+	{
+		"nvim-mini/mini.ai",
 		version = false,
 		opts = {},
 	},
