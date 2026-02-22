048c08ead67a761c29765d20e49676645872b72c
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-02-22 18:09:54 -0500

    Update mini.pairs mappings in mini.lua

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -9,7 +9,49 @@ return {
 	{
 		"nvim-mini/mini.pairs",
 		version = false,
-		opts = {},
+		opts = {
+			mappings = {
+				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+				["["] = {
+					action = "open",
+					pair = "[]",
+					neigh_pattern = ".[%s%z%)}%]]",
+					register = { cr = false },
+				},
+				["{"] = {
+					action = "open",
+					pair = "{}",
+					neigh_pattern = ".[%s%z%)}%]]",
+					register = { cr = false },
+				},
+				["("] = {
+					action = "open",
+					pair = "()",
+					neigh_pattern = ".[%s%z%)]",
+					register = { cr = false },
+				},
+				['"'] = {
+					action = "closeopen",
+					pair = '""',
+					neigh_pattern = "[^%w\\][^%w]",
+					register = { cr = false },
+				},
+				["'"] = {
+					action = "closeopen",
+					pair = "''",
+					neigh_pattern = "[^%w\\][^%w]",
+					register = { cr = false },
+				},
+				["`"] = {
+					action = "closeopen",
+					pair = "``",
+					neigh_pattern = "[^%w\\][^%w]",
+					register = { cr = false },
+				},
+			},
+		},
 	},
 	{
 		"nvim-mini/mini.indentscope",
