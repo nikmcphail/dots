7442b7f64fc73c0f74c0d8d7c8178c33f6aa4f8c
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-03-08 05:07:33 -0400

    Move vimscape to fun-plugins, along with wrapped

---
+++ nvim/.config/nvim/lua/plugins/fun-plugins.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"jakewaldrip/vimscape2007.nvim",
+		version = "*",
+		opts = {},
+	},
+	{
+		"aikhe/wrapped.nvim",
+		dependencies = { "nvzone/volt" },
+		cmd = { "WrappedNvim" },
+		opts = {
+			border = true,
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/vimscape.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"jakewaldrip/vimscape2007.nvim",
-	version = "*",
-	opts = {},
-}
