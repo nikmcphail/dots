dee1bb9b0f9285d8714e771e23cde633037b54de
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-03-12 03:05:52 -0400

    Add bind to toggle transprancy in neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,20 +3,31 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function()
-			require("rose-pine").setup({
-				variant = "moon",
-				enable = {
-					terminal = true,
-					legacy_highlights = true,
-					migrations = true,
-				},
-				styles = {
-					transparency = true,
-					italic = true,
-				},
-			})
+			vim.g.rosepine_transparent = true
 
-			vim.cmd("colorscheme rose-pine")
+			local function reload_theme()
+				require("rose-pine").setup({
+					variant = "moon",
+					enable = {
+						terminal = true,
+						legacy_highlights = true,
+						migrations = true,
+					},
+					styles = {
+						transparency = vim.g.rosepine_transparent,
+						italic = true,
+					},
+				})
+
+				vim.cmd.colorscheme("rose-pine")
+			end
+
+			reload_theme()
+
+			vim.keymap.set("n", "<leader>O", function()
+				vim.g.rosepine_transparent = not vim.g.rosepine_transparent
+				reload_theme()
+			end, { desc = "Toggle opacity" })
 		end,
 	},
 }
