cbd4f80a2ee1eaf0628b2bf0851ae72a84c2bb60
Nik McPhail <nikolasstevenmcphail@gmail.com>
2026-03-12 00:24:22 -0400

    back to rose-pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,22 @@
 return {
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			variant = "moon",
-	-- 			enable = {
-	-- 				terminal = true,
-	-- 				legacy_highlights = true,
-	-- 				migrations = true,
-	-- 			},
-	-- 			styles = {
-	-- 				transparency = true,
-	-- 				italic = true,
-	-- 			},
-	-- 		})
-	--
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
 	{
-		dir = "/home/nik/Documents/Repos/Lua/coffee",
+		"rose-pine/neovim",
+		name = "rose-pine",
 		config = function()
-			require("coffee").setup({
+			require("rose-pine").setup({
+				variant = "moon",
+				enable = {
+					terminal = true,
+					legacy_highlights = true,
+					migrations = true,
+				},
 				styles = {
 					transparency = true,
+					italic = true,
 				},
 			})
-			vim.cmd("colorscheme coffee")
+
+			vim.cmd("colorscheme rose-pine")
 		end,
 	},
 }
